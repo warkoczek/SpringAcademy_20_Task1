1f23180599f8aaaa8480bd38441284c08135ab1b
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-10-30 18:19:04 +0100

    first commit

---
+++ src/main/java/pl/warkoczewski/SpringAcademy_20_Task1/SpringAcademy20Task1Application.java
@@ -0,0 +1,13 @@
+package pl.warkoczewski.SpringAcademy_20_Task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademy20Task1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAcademy20Task1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/warkoczewski/SpringAcademy_20_Task1/SpringAcademy20Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.warkoczewski.SpringAcademy_20_Task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademy20Task1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
